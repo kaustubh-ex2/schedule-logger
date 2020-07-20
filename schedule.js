cc440448fa33d1d8cd3f0a6133c51fad8cf9e93a
kaustubh-ex2 <60777148+kaustubh-ex2@users.noreply.github.com>
2020-07-20 12:51:40 +0530

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -12,7 +12,10 @@ const csvWriter = createCsvWriter({
         {id: 'Status', title: 'Status'}
     ]
 });
-const BASEURL = "https://stage-hlapi.newhomesource.com/";
+const BASEURL = {
+	stage: "https://stage-hlapi.newhomesource.com/",
+	sprint: "https://sprint-hl-api.newhomesource.com/"
+};
 const requests = [
     `api/v3/graphQL?partnerId=&sessionToken=&query={%20searchImages(partnerId:%209383,%20minImgWidth:%20400,%20minImgHeight:%20300,%20sflow:%20500,%20sfhigh:%2010000,%20pageSize:%2022,%20page:%201,%20excludeBasiCommunities:%20true,%20client:%20%22HomLuv%22,%20imgType:%20%22ELE,INT%22,%20imgCat:%20%22Bedroom%22,%20imgColorType:%20%22TrueColor%22)%20{%20id%20commId%20communityName%20planId%20planName%20listingId%20brHi%20homeId%20garageHi%20builderName%20baHi%20sftHi%20city%20state%20addr%20isSpec%20brand%20{%20id%20name%20logo%20logoSmall%20marketId%20state%20siteUrl%20}%20phone%20imageInfo%20{%20id%20path%20category%20objectLabels%20}%20prHi%20intImage%20thumb%20thumb2%20isBasic%20builderId%20county%20marketId%20marketName%20halfBa%20stories%20zip%20isLiked%20isSaved%20imageBookmarkDataId%20homeBookmarkDataId%20}%20}`,
     `api/v3/graphQL?partnerId=&sessionToken=&query={%20searchImages(partnerId:%209383,%20minImgWidth:%20400,%20minImgHeight:%20300,%20sflow:%20500,%20sfhigh:%2010000,%20pageSize:%2022,%20page:%201,%20excludeBasiCommunities:%20true,%20client:%20%22HomLuv%22,%20imgType:%20%22ELE,INT%22,%20imgCat:%20%22Bathroom%22,%20imgColorType:%20%22TrueColor%22)%20{%20id%20commId%20communityName%20planId%20planName%20listingId%20brHi%20homeId%20garageHi%20builderName%20baHi%20sftHi%20city%20state%20addr%20isSpec%20brand%20{%20id%20name%20logo%20logoSmall%20marketId%20state%20siteUrl%20}%20phone%20imageInfo%20{%20id%20path%20category%20objectLabels%20}%20prHi%20intImage%20thumb%20thumb2%20isBasic%20builderId%20county%20marketId%20marketName%20halfBa%20stories%20zip%20isLiked%20isSaved%20imageBookmarkDataId%20homeBookmarkDataId%20}%20}`,
@@ -35,13 +38,14 @@ const requests = [
 const j = cron.scheduleJob("*/10 * * * *", function(){
     JustDoIt();
 });
+console.log("Job started...");
 
 let SnoCounter = 0;
 function JustDoIt()
 {
     for(let i = 0; i < requests.length; i++){
         setTimeout(function(){
-            const url = BASEURL + requests[i];
+            const url = BASEURL.sprint + requests[i];
             
             var dataToAppend = [{
                 Sno: ++SnoCounter,
@@ -55,9 +59,10 @@ function JustDoIt()
                 return raw.json();
             })
             .then(res => {
-				console.log("Response not received at", dataToAppend[0].ResTime);
+				console.log("Response received at", dataToAppend[0].ResTime);
                 if(res.Status.toLowerCase() != "ok" ||  !res.Result || !res.Result.searchImages || res.Result.searchImages.length == 0){
                     dataToAppend[0].Res = JSON.stringify(res);
+					console.log("Failed");
                     csvWriter.writeRecords(dataToAppend);
                 }
             })
